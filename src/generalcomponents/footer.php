8fab2c583c3212ea0c237207b2ef713265e294fe
juangienn <104360308+juangienn@users.noreply.github.com>
2024-10-21 08:43:59 -0700

    test

---
+++ src/generalcomponents/footer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Water Conservation</title>
+    <link rel="stylesheet" href="../../assets/css/style.css">
+</head>
+<body>
+
+    <!-- Main content would go here -->
+
+    <footer>
+        <div class="footer-container">
+            <p>&copy; 2024 Water Conservation. All rights reserved.</p><br>
+            <p>&copy; For more information, Please contact us @ gibertjohn0325@gmail.com.</p>
+            <ul class="footer-links">
+                <li><a href="privacy_policy.php">Privacy Policy</a></li>
+                <li><a href="../generalcomponents/terms_of_service.php">Terms of Service</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ src/generalcomponents/header.php
@@ -0,0 +1,26 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Water Conservation Platform</title>
+    <link rel="stylesheet" href="../../assets/css/style.css"> <!-- Link to external CSS file -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <img src="../../assets/images/logo.jfif" alt="Water Conservation Logo" class="logo">
+            <nav>
+                <ul class="menu-bar">
+                    <li><a href="../homepage/index.php"><i class="fas fa-home"></i></a></li>
+                    <li><a href="../supportpage/help.php"><i class="fas fa-question-circle"></i></a></li>
+                    <li><a href="../contactpage/contact.php"><i class="fas fa-envelope"></i></a></li>
+                </ul>
+            </nav>
+
+        </div>
+    </header>
+</body>
+</html>
